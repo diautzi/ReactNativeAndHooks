1656a1054a5fbd7ed73a52b428187a45ac1ecdd8
Diana Utan <didiutzi@gmail.com>
2021-02-08 12:48:08 -0600

    setup and connection established with mondoDB

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+const mongoUri = 'mongodb+srv://diana:utan@cluster0.uvmiv.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance')
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err)
+})
 app.get('/', (req, res) => {
     res.send('Hi there!');
 });
